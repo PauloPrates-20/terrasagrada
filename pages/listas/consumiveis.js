2efa3fe81a1dadee9904de1e75271e0a35c4ca69
PauloPrates-20 <paulo.prates01@hotmail.com>
2024-01-08 19:04:23 -0300

    correcao das paginas de listas

--- pages/listas/consumiveis.js
+++ pages/listas/consumiveis.js
@@ -3,42 +3,46 @@ import { getData } from "@/lib/getDbData"
 
 // test data
 
-const data = [{ id: 0, item: 'teste', tier: 'teste', url: 'www.google.com', value: 'teste' }]
+// const data = [{ id: 0, item: 'teste', tier: 'teste', url: 'www.google.com', value: 'teste' }]
 
-const commonData = data
-const uncommonData = data
-const rareData = data
-const veryRareData = data
-const legendaryData = data
+// const commonData = data
+// const uncommonData = data
+// const rareData = data
+// const veryRareData = data
+// const legendaryData = data
 
-const dataSet = [
-  { id: 0, docData: commonData, tier: 'comum' },
-  { id: 1, docData: uncommonData, tier: 'incomum' },
-  { id: 2, docData: rareData, tier: 'raro' },
-  { id: 3, docData: veryRareData, tier: 'muRaro' },
-  { id: 4, docData: legendaryData, tier: 'lendario' }
-]
+// const dataSet = [
+//   { id: 0, docData: commonData, tier: 'comum' },
+//   { id: 1, docData: uncommonData, tier: 'incomum' },
+//   { id: 2, docData: rareData, tier: 'raro' },
+//   { id: 3, docData: veryRareData, tier: 'muRaro' },
+//   { id: 4, docData: legendaryData, tier: 'lendario' }
+// ]
 
-// export async function getStaticProps() {
-//   const tiers = ['comum', 'incomum', 'raro', 'muRaro', 'lendario']
-//   const commonData = await getData('Consumiveis', tiers[0])
-//   const uncommonData = await getData('Consumiveis', tiers[1])
-//   const rareData = await getData('Consumiveis', tiers[2])
-//   const veryRareData = await getData('Consumiveis', tiers[3])
-//   const legendaryData = await getData('Consumiveis', tiers[4])
+export async function getStaticProps() {
+  const tiers = ['comum', 'incomum', 'raro', 'muRaro', 'lendario']
+  const commonData = await getData('Consumiveis', tiers[0])
+  const uncommonData = await getData('Consumiveis', tiers[1])
+  const rareData = await getData('Consumiveis', tiers[2])
+  const veryRareData = await getData('Consumiveis', tiers[3])
+  const legendaryData = await getData('Consumiveis', tiers[4])
 
-//   return {
-//     props: {
-//       commonData,
-//       uncommonData,
-//       rareData,
-//       veryRareData,
-//       legendaryData
-//     }
-//   }
-// }
+  const dataSet = [
+    { id: 0, docData: commonData, tier: 'comum' },
+    { id: 1, docData: uncommonData, tier: 'incomum' },
+    { id: 2, docData: rareData, tier: 'raro' },
+    { id: 3, docData: veryRareData, tier: 'muRaro' },
+    { id: 4, docData: legendaryData, tier: 'lendario' }
+  ]
 
-export default function Consumables() {
+  return {
+    props: {
+      dataSet
+    }
+  }
+}
+
+export default function Consumables({ dataSet }) {
   return (
     <>
       <ListPage data={dataSet} type='potion' title='Consumíveis' />
--- pages/listas/montarias.js
+++ pages/listas/montarias.js
@@ -3,21 +3,25 @@ import { getData } from '@/lib/getDbData'
 import ListPage from '@/components/ListPage'
 
 // test data
-const data = [{id: 0, item: 'teste', tier: 'teste', url: 'www.google.com', value: 'teste'}]
 
-const dataSet = [
-  {id: 0, docData: data, tier: 'comum'},
-]
+// const data = [{id: 0, item: 'teste', tier: 'teste', url: 'www.google.com', value: 'teste'}]
 
-// export async function getStaticProps() {
-//   const data = await getData('Montarias', 'comum')
+// const dataSet = [
+//   {id: 0, docData: data, tier: 'comum'},
+// ]
 
-//   return {
-//     props: { data }
-//   }
-// }
+export async function getStaticProps() {
+  const data = await getData('Montarias', 'comum')
+  const dataSet = [
+    {id: 0, docData: data, tier: 'comum'}
+  ]
 
-export default function Mounts() {
+  return {
+    props: { dataSet }
+  }
+}
+
+export default function Mounts({ dataSet }) {
   return (
     <>
       <ListPage data={dataSet} title='Montarias' type='horse' />
