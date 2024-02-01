a0669cfb1f295bd6ceabea915b38ca80dcc52531
PauloPrates-20 <paulo.prates01@hotmail.com>
2024-02-01 13:05:01 -0300

    correcao de deploy

--- pages/gmtools/index.js
+++ pages/gmtools/index.js
@@ -1,6 +1,6 @@
 import { firebaseDb } from "@/lib/firestoreConfig"
 import { doc, setDoc } from 'firebase/firestore/lite'
-import { useRouter } from 'next/router'
+import { useRouter } from "next/navigation"
 import { getAllData, getData } from '@/lib/getDbData'
 
 // import databaseData from
