c3729d31d0fb16bfcc7cc2bf878aeafcb10e885d
Paulo Prates <paulo.prates01@hotmail.com>
2025-03-18 15:06:44 -0300

    fixed navbar dropdown (i think)

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -31,7 +31,7 @@ export default function Dropdown() {
 					className='flex cursor-pointer mt-1 ml-3 mb-3 justify-start items-end'
 				/>
 				<div
-					className={`flex flex-col absolute left-0 top-4 mt-12 pb-4 bg-barColor w-full sm:w-72 max-h-0 opacity-0 invisible rounded-[0_0_6px_0] transition-all ease-out duration-200 ${display ? 'isChecked' : 'notChecked'}`}
+					className={`flex flex-col absolute left-0 top-3 mt-10 pb-4 bg-barColor w-full sm:w-72 max-h-0 opacity-0 invisible rounded-[0_0_6px_0] transition-all ease-out duration-200 ${display ? 'isChecked' : 'notChecked'}`}
 				>
 					<a 
 						href='/'
