495a5e3057a32a656888919bf90f1c54f3a01849
Paulo Prates <paulo.prates01@hotmail.com>
2025-03-19 10:00:27 -0300

    fixed user object params

--- auth.ts
+++ auth.ts
@@ -2,10 +2,26 @@ import NextAuth from 'next-auth';
 import Discord from 'next-auth/providers/discord';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Discord({
-        authorization: {
-            url: "https://discord.com/oauth2/authorize",
-            params: { scope: 'identify', prompt: 'none' }
-        }
-    })],
+  providers: [Discord({
+    authorization: {
+      url: "https://discord.com/oauth2/authorize", // must configure base url but the documentation doesn't say it
+      params: { scope: 'identify', prompt: 'none' },
+    },
+  })],
+  callbacks: {
+    jwt({ token, profile }) {
+      if (profile) {
+        // console.log('[AUTH][USER]: Logging user info\n', profile);
+        token.id = profile.id; // gets the actual discord snowflake id (user's is encrypted for some reason)
+        delete token.email; // deletes the email, even though the 'email' scope is not being passed
+      }
+
+      return token;
+    },
+    session({ session, token }) {
+      // console.log('[AUTH][USER]: Logging session user info\n' + JSON.stringify(token, null, 2));
+      session.user.id = String(token.id);
+      return session;
+    }
+  }
 });
