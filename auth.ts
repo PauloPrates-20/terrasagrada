6c71c5f98a4552c77615c92bb266b6f6098bb65b
Paulo Prates <paulo.prates01@hotmail.com>
2025-03-20 09:42:29 -0300

    updated session params

--- auth.ts
+++ auth.ts
@@ -1,5 +1,13 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
 import Discord from 'next-auth/providers/discord';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      accessToken: string
+    } & DefaultSession['user']
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord({
@@ -9,19 +17,24 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   })],
   callbacks: {
-    jwt({ token, profile }) {
+    jwt({ token, profile, account }) {
       if (profile) {
         // console.log('[AUTH][USER]: Logging user info\n', profile);
         token.id = profile.id; // gets the actual discord snowflake id (user's is encrypted for some reason)
         delete token.email; // deletes the email, even though the 'email' scope is not being passed
       }
 
+      if (account?.provider === 'discord') {
+        token.accessToken = account.access_token;
+      }
+
       return token;
     },
     session({ session, token }) {
+      session.user.id = String(token.id);
+      session.user.accessToken = String(token.accessToken);
       // console.log('[AUTH][USER]: Logging session user info\n' + JSON.stringify(token, null, 2));
       // console.log('[AUTH][SESSION] Logging session information\n', JSON.stringify(session, null, 2));
-      session.user.id = String(token.id);
       return session;
     }
   },
