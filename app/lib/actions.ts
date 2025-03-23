a44d4d7fd5dc792ac00e9d3a9c495d290f64733c
Paulo Prates <paulo.prates01@hotmail.com>
2025-03-22 23:45:33 -0300

    V4.4.0: Compras e Vendas

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,7 +10,7 @@ export async function buyItem(item: Item): Promise<any> {
   }
   const accessToken = session.user.accessToken;
 
-  const response = await fetch('http://localhost:5000/api/buy', {
+  const response = await fetch(`${process.env.DISCORD_API_URL}/buy`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ accessToken: accessToken, item: item })
