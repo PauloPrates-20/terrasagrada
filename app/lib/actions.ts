57a0f66a10c726b962bf7c12d2bd21697015f102
Paulo Prates <paulo.prates01@hotmail.com>
2026-02-08 22:20:15 -0300

    fixed buy button

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,30 +4,30 @@ import { Item } from './definitions'
 import { auth } from '@/auth';
 
 export async function getId() {
-  const session = await auth();
+    const session = await auth();
 
-  if (!session?.user) return { error: 'Usuário não autenticado.' };
+    if (!session?.user) return { error: 'Usuário não autenticado.' };
 
-  return session.user.id as string;
+    return session.user.id as string;
 }
 
 export async function buyItem(item: Item, characterKey: string): Promise<any> {
-  const session = await auth();
-  if (!session?.user) {
-    return { error: 'Faça login para comprar!' };
-  }
+    const session = await auth();
+    if (!session?.user) {
+        return { error: 'Faça login para comprar!' };
+    }
 
-  const accessToken = session.user.accessToken;
-  try {
-    const response = await fetch(`${process.env.API_URL}/buy`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ accessToken, item, characterKey })
-    });
-    const data = await response.json();
-  
-    return data;
-  } catch (e: any) {
-    return { error: `Erro de servidor: ${e.message}` };
-  }
+    const accessToken = session.user.accessToken;
+    try {
+        const response = await fetch(`${process.env.API_URL}/buy`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ accessToken, item, characterKey })
+        });
+        const data = await response.json();
+
+        return data;
+    } catch (e: any) {
+        return { error: `Erro de servidor: ${e.message}` };
+    }
 }
