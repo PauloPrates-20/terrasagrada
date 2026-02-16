d4490f2a8fd81aca50c9b77a4132c8c4097bdb9f
Paulo Prates <147115682+PauloPrates-20@users.noreply.github.com>
2026-02-15 23:46:41 -0300

    V5.0.2: Adjusted prices
    * updated item lists
    
    * V5.0.0: Revamped item lists
    
    * fixed buy button
    
    * fixed mundane items
    
    * fix security alerts
    
    * fixed lock file
    
    * updated item lists
    
    * fixed empty rendering
    
    * adjusted prices

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -5,50 +5,50 @@ import { FaBars } from 'react-icons/fa';
 import NavList, { Options } from './NavList';
 
 export default function Dropdown() {
-	const [display, setDisplay] = useState(false);
+    const [display, setDisplay] = useState(false);
 
-	const navLists: Options[] = [
-		{ name: 'Consumíveis', url: '/listas/consumiveis' },
-		{ name: 'Itens Mágicos', url: '/listas/itensmagicos'},
-		{ name: 'Itens Mundanos', url: '/listas/mundanos'},
-		{ name: 'Infusões de Artífice', url: '/listas/artifice'}
-	];
+    const navLists: Options[] = [
+        { name: 'Consumíveis', url: '/listas/consumiveis' },
+        { name: 'Itens Mágicos', url: '/listas/itensmagicos' },
+        { name: 'Itens Mundanos', url: '/listas/mundanos' },
+        { name: 'Variantes Mágicas', url: '/listas/variante' }
+    ];
 
-	const navTools: Options[] = [
-		{ name: 'Calculadora de HP', url: '/ferramentas/calculadorahp' },
-		{ name: 'Nível de Conjurador', url: '/ferramentas/conjurador' },
-		{ name: 'Calculadora do Artesão', url: '/ferramentas/crafting' },
-		{ name: 'Grimório do Mago', url: '/ferramentas/grimorio' }
-	];
+    const navTools: Options[] = [
+        { name: 'Calculadora de HP', url: '/ferramentas/calculadorahp' },
+        { name: 'Nível de Conjurador', url: '/ferramentas/conjurador' },
+        { name: 'Calculadora do Artesão', url: '/ferramentas/crafting' },
+        { name: 'Grimório do Mago', url: '/ferramentas/grimorio' }
+    ];
 
-	return (
-		<>
-			<nav
-				className='absolute left-0 flex flex-row flex-wrap justify-start w-full'
-			>
-				<FaBars 
-					onClick={() => setDisplay(!display)}
-					className='flex cursor-pointer mt-1 ml-3 mb-3 justify-start items-end'
-				/>
-				<div
-					className={`flex flex-col absolute left-0 top-3 mt-10 pb-4 bg-barColor w-full sm:w-72 max-h-0 opacity-0 invisible rounded-[0_0_6px_0] transition-all ease-out duration-200 ${display ? 'isChecked' : 'notChecked'}`}
-				>
-					<a 
-						href='/'
-						className='select-none relative no-underline text-xl text-textColor py-3 px-4 mx-0 w-full hover:shadow-[0_0_24px_8px_inset_#4406067f]'
-					>
-						Home
-					</a>
-          <a
-            href='http://vtt.terrasagrada.net.br'
-						className='select-none relative no-underline text-xl text-textColor py-3 px-4 mx-0 w-full hover:shadow-[0_0_24px_8px_inset_#4406067f]'
-          >
-            Foundry
-          </a>
-					<NavList label='Listas' options={navLists} />
-					<NavList label='Ferramentas' options={navTools} />
-				</div>
-			</nav>
-		</>
-	);
+    return (
+        <>
+            <nav
+                className='absolute left-0 flex flex-row flex-wrap justify-start w-full'
+            >
+                <FaBars
+                    onClick={() => setDisplay(!display)}
+                    className='flex cursor-pointer mt-1 ml-3 mb-3 justify-start items-end'
+                />
+                <div
+                    className={`flex flex-col absolute left-0 top-3 mt-10 pb-4 bg-barColor w-full sm:w-72 max-h-0 opacity-0 invisible rounded-[0_0_6px_0] transition-all ease-out duration-200 ${display ? 'isChecked' : 'notChecked'}`}
+                >
+                    <a
+                        href='/'
+                        className='select-none relative no-underline text-xl text-textColor py-3 px-4 mx-0 w-full hover:shadow-[0_0_24px_8px_inset_#4406067f]'
+                    >
+                        Home
+                    </a>
+                    <a
+                        href='http://vtt.terrasagrada.net.br'
+                        className='select-none relative no-underline text-xl text-textColor py-3 px-4 mx-0 w-full hover:shadow-[0_0_24px_8px_inset_#4406067f]'
+                    >
+                        Foundry
+                    </a>
+                    <NavList label='Listas' options={navLists} />
+                    <NavList label='Ferramentas' options={navTools} />
+                </div>
+            </nav>
+        </>
+    );
 }
