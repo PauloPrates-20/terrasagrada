cd3e226911659d088f3c4c1759c7844a968f2fad
Paulo Prates <paulo.prates01@hotmail.com>
2026-03-07 13:23:01 -0300

    V6.0.0: updated shop API and database

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,33 +5,33 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const quantico = Quantico({
-	subsets: ['latin'],
-	weight: ['400', '700']
+    subsets: ['latin'],
+    weight: ['400', '700']
 });
 
 export const metadata: Metadata = {
-	title: {
-		template: 'Terra Sagrada - %s',
-		default: 'Terra Sagrada'
-	}
+    title: {
+        template: 'Terra Sagrada - %s',
+        default: 'Terra Sagrada'
+    }
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body
-        className={`${quantico.className} antialiased bg-fixed bg-cover text-textColor min-h-dvh w-full`}
-      >
-				<Navbar />
-				<main className='min-h-fit h-fit sm:min-h-[700px] mt-60 mb-40 bg-bgColor text-center p-4 shadow-[0_0_30px_50px_#0b0a13]'>
-        	{children}
-				</main>
-				<Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body
+                className={`${quantico.className} antialiased bg-fixed bg-cover text-textColor min-h-dvh w-full`}
+            >
+                <Navbar />
+                <main className='min-h-fit h-fit sm:min-h-[700px] mt-60 mb-40 bg-bgColor text-center p-4 shadow-[0_0_30px_50px_#0b0a13]'>
+                    {children}
+                </main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
